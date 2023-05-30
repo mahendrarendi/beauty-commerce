2ae60fe43b0322d177287831c77e8b113c425273
RENDI NICOLAS MAHENDRA <mahendrarendi@RENDIs-Air.lan>
2023-05-30 19:16:41 +0700

    rollback pembeli page

--- pembeli/login.php
+++ pembeli/login.php
@@ -1,72 +1,72 @@
 <!doctype html>
-<html class="no-js" lang="en">
-
+<html lang="zxx">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Beauty-Commerce</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- favicon
-		============================================ -->
-  <link rel="shortcut icon" type="image/x-icon" href="img/logo/logo.png">
-  <!-- Google Fonts
-		============================================ -->
-  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
-  <!-- Bootstrap CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <!-- Bootstrap CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/font-awesome.min.css">
-  <!-- owl.carousel CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/owl.carousel.css">
-  <link rel="stylesheet" href="css/owl.theme.css">
-  <link rel="stylesheet" href="css/owl.transitions.css">
-  <!-- animate CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/animate.css">
-  <!-- normalize CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <!-- main CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/main.css">
-  <!-- morrisjs CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/morrisjs/morris.css">
-  <!-- mCustomScrollbar CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/scrollbar/jquery.mCustomScrollbar.min.css">
-  <!-- metisMenu CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/metisMenu/metisMenu.min.css">
-  <link rel="stylesheet" href="css/metisMenu/metisMenu-vertical.css">
-  <!-- calendar CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/calendar/fullcalendar.min.css">
-  <link rel="stylesheet" href="css/calendar/fullcalendar.print.min.css">
-  <!-- forms CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/form/all-type-forms.css">
-  <!-- style CSS
-		============================================ -->
-  <link rel="stylesheet" href="style.css">
-  <!-- responsive CSS
-		============================================ -->
-  <link rel="stylesheet" href="css/responsive.css">
-  <!-- modernizr JS
-		============================================ -->
-  <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Kerajinanku</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="../admin/img/logo/logo.png">
+
+    <!-- CSS here -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/flaticon.css">
+        <link rel="stylesheet" href="assets/css/slicknav.css">
+        <link rel="stylesheet" href="assets/css/animate.min.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+        <link rel="stylesheet" href="assets/css/themify-icons.css">
+        <link rel="stylesheet" href="assets/css/slick.css">
+        <link rel="stylesheet" href="assets/css/nice-select.css">
+        <link rel="stylesheet" href="assets/css/style.css">
 </head>
-
 <body>
-  <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-  <div class="container-fluid">
+    <header>
+        <!-- Header Start -->
+        <div class="header-area">
+            <div class="main-header header-sticky">
+                <div class="container-fluid">
+                    <div class="menu-wrapper">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.html"><img src="../admin/img/logo/logo.png" alt="" style="height: 75px;"></a>
+                        </div>
+                        <!-- Main-menu -->
+                        <div class="main-menu d-none d-lg-block">
+                            <nav>                                                
+                                <ul id="navigation">  
+                                <li><a href="../index.php"><b>Beranda</b></a></li>
+                                    <li><a href="../index.php#produk"><b>Produk</b></a></li>
+                                    <li><a href="../index.php#tentang"><b>Tentang</b></a></li>
+                                    <li><a href="../index.php#kontak"><b>Kontak</b></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <!-- Header Right -->
+                        <div class="header-right main-menu d-none d-lg-block">
+                            <nav>
+                            <ul id="navigation">  
+                                <li><a href="daftar.php" style="margin-top: 10px;"><b>Daftar</b></a></li>
+                                <li><a href="login.php"><button class="btn_3"><b>Login</b></button></a></li>
+                            </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <!-- Mobile Menu -->
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+    </header>
+    <main style="background-color: #f0f0f0;">
+        <!--================login_part Area =================-->
+        <section class="login_part section_padding ">
+        <div class="container-fluid">
     <div class="row">
       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"></div>
       <div class="col-md-4 col-md-4 col-sm-4 col-xs-12">
@@ -94,63 +94,46 @@
     </div>
 
   </div>
-  <!-- <div class="footer-copyright-area" style="margin-top: auto ;">
-      <div class="container-fluid">
-          <div class="row">
-              <div class="col-12">
-                  <div class="footer-copy-right">
-                      <p>Copyright © 2023 Beauty-Commerce,</a> All rights reserved.</p>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div> -->
+        </section>
+        <!--================login_part end =================-->
+    </main>
+    <!-- Search model end -->
+    
+    <!-- JS here -->
 
-  <!-- jquery
-		============================================ -->
-  <script src="js/vendor/jquery-1.11.3.min.js"></script>
-  <!-- bootstrap JS
-		============================================ -->
-  <script src="js/bootstrap.min.js"></script>
-  <!-- wow JS
-		============================================ -->
-  <script src="js/wow.min.js"></script>
-  <!-- price-slider JS
-		============================================ -->
-  <script src="js/jquery-price-slider.js"></script>
-  <!-- meanmenu JS
-		============================================ -->
-  <script src="js/jquery.meanmenu.js"></script>
-  <!-- owl.carousel JS
-		============================================ -->
-  <script src="js/owl.carousel.min.js"></script>
-  <!-- sticky JS
-		============================================ -->
-  <script src="js/jquery.sticky.js"></script>
-  <!-- scrollUp JS
-		============================================ -->
-  <script src="js/jquery.scrollUp.min.js"></script>
-  <!-- mCustomScrollbar JS
-		============================================ -->
-  <script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
-  <script src="js/scrollbar/mCustomScrollbar-active.js"></script>
-  <!-- metisMenu JS
-		============================================ -->
-  <script src="js/metisMenu/metisMenu.min.js"></script>
-  <script src="js/metisMenu/metisMenu-active.js"></script>
-  <!-- tab JS
-		============================================ -->
-  <script src="js/tab.js"></script>
-  <!-- icheck JS
-		============================================ -->
-  <script src="js/icheck/icheck.min.js"></script>
-  <script src="js/icheck/icheck-active.js"></script>
-  <!-- plugins JS
-		============================================ -->
-  <script src="js/plugins.js"></script>
-  <!-- main JS
-		============================================ -->
-  <script src="js/main.js"></script>
+    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
+
+    <!-- One Page, Animated-HeadLin -->
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/animated.headline.js"></script>
+    
+    <!-- Scroll up, nice-select, sticky -->
+    <script src="./assets/js/jquery.scrollUp.min.js"></script>
+    <script src="./assets/js/jquery.nice-select.min.js"></script>
+    <script src="./assets/js/jquery.sticky.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- contact js -->
+    <script src="./assets/js/contact.js"></script>
+    <script src="./assets/js/jquery.form.js"></script>
+    <script src="./assets/js/jquery.validate.min.js"></script>
+    <script src="./assets/js/mail-script.js"></script>
+    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+    
+    <!-- Jquery Plugins, main Jquery -->	
+    <script src="./assets/js/plugins.js"></script>
+    <script src="./assets/js/main.js"></script>
+
 </body>
-
+    
 </html>
