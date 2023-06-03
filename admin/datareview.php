26a4a128b3d77cfec6ae22a1248bdaf9aabcedfd
RENDI NICOLAS MAHENDRA <mahendrarendi@RENDIs-Air.lan>
2023-06-03 19:21:56 +0700

    change datariview

--- admin/datareview.php
+++ admin/datareview.php
@@ -11,7 +11,7 @@ $id = @$_REQUEST['admin_id'];
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Beauty-Commerce</title>
+    <title>Kerajinanku</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
@@ -257,7 +257,6 @@ $id = @$_REQUEST['admin_id'];
                                             echo "<td>$d[produk_nama]</td>";
                                             echo "<td>$d[review_isi]</td>";
                                             echo "<td><i class='fa fa-star' style='color: #24CAA1;'>$d[review_rating]</td></i>";
-                                            echo "<td><a href='datareview_edit.php?id=$d[review_id]'><button data-toggle='tooltip' title='Edit' class='pt-setting'><i class='fa fa-pencil-square-o' aria-hidden='true'></i></button></a></td>";
                                             echo "<td><a href='#' onclick=\"javascript: if(confirm('Apakah review dari" . addslashes($d['pembeli_nama']) . " untuk produk " . addslashes($d['produk_nama']) . " mau dihapus?')==true)
                                            {window.location.href='datareview_proses.php?aksi=hapus&tid=$d[review_id]'; } \"><button data-toggle='tooltip' title='Hapus' class='ds-setting'><i class='fa fa-trash-o' aria-hidden='true'></i></button></a></td>";
                                             echo "</td>";
@@ -287,7 +286,7 @@ $id = @$_REQUEST['admin_id'];
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="footer-copy-right">
-                            <p>Copyright © 2023 Beauty-Commerce, All rights reserved.</p>
+                            <p>Copyright © 2023 Kerajinanku, All rights reserved.</p>
                         </div>
                     </div>
                 </div>
